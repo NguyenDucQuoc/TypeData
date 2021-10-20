cca9c738117ef893169bb63880bbf55703bcd0f7
Nguyen Duc Quoc <nguyenducquoc2003@gmail.com>
2021-10-20 16:38:03 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,46 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+	/**
+	 *  khai bao thong tin sinh vien
+	 */
+	System.out.println("thong tin mot sinh vien");
+	String ten = "Nguyen Duc Quoc";
+	System.out.println("Ten sinh vien: " + ten);
+	String msv = "2160176";
+	System.out.println("Ma sinh vien" + msv);
+	String cccd = "01215759960";
+	System.out.println("can cuoc cong dan: " + cccd);
+	String tenlop = "K21CDTH41";
+	System.out.println("ten lop:" + tenlop);
+	String tentruong = "hueic";
+	System.out.println("ten truong:" + tentruong);
+	String dtb = "8.5";
+	System.out.println("diem trung binh " + dtb);
+	System.out.println("=================================================");
+	
+	 /**
+	 * khai bao thong tin ca nhan
+	 */
+	String tcn = "Quoc";
+	System.out.println("ten cong nhan:" + tcn);
+	String dc ="70nguyenhue";
+	System.out.println("dia chi:" + dc);
+	String hsl = "500";
+	System.out.println("he so luong:" + hsl);
+	System.out.println("===================================================");
+	/**
+	 * thong tin mot chiec xe
+	 */
+	String tenthongtin = "DucQuoc";
+	System.out.println("ten thong tin:" + tenthongtin);
+	String hang ="Ducati";
+	System.out.println("hang xe:" + hang);
+	String gia = "100000000";
+	System.out.println("gia xe:" + gia);
+	String mauson ="do";
+	System.out.println("mau son:" + mauson);   
+	}
+}	
+
